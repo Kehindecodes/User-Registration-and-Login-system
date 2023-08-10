303a5afdeacbca44d948967043d24835a3b9bbe7
Oki Kehinde <olamiposi.oki@gmail.com>
2023-08-10 16:43:52 +0100

    create authentication middleware

---
+++ middleware/AuthMiddleware.php
@@ -0,0 +1,18 @@
+<?php
+
+class AuthMiddleware{
+       /**
+     * Requires authentication for access.
+     *
+     * @throws Exception Redirects to the login page if user is not authenticated.
+     * @return void
+     */
+    public static function requireAuth(){
+        session_start();
+        if(!isset($_SESSION['user_id'])){
+            // if not logged in redirect to login page
+            header('Location: /login');
+            exit;
+        }
+    }
+}
