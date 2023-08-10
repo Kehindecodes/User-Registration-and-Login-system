bb67e441ad9f2a69d60877460a6017bb44318b2b
Oki Kehinde <olamiposi.oki@gmail.com>
2023-08-10 13:20:57 +0100

    create profile page

---
+++ app/views/profile.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile Page</title>
+    <!-- Include Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <!-- link css -->
+    <link rel="stylesheet" href="./app.css">
+</head>
+
+<body>
+    <div class="container py-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card profile-card">
+                    <div class="card-body">
+                        <img src="profile-image.jpg" alt="Profile Image" class="profile-image">
+                        <h4 class="profile-name">John Doe</h4>
+                        <p class="bio">Passionate about coding, design, and coffee!</p>
+                        <hr>
+                        <div class="contact-info">
+                            <p><strong>Email:</strong> john@example.com</p>
+                            <p><strong>Password:</strong> *********</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
